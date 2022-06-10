a4f27eb9084c24d8617f2ab134a63af03ac0edb3
damanh1905 <18130005@st.hcmuaf.edu.vn>
2022-06-10 22:39:41 +0700

    Latest News

---
+++ Fe/src/pages/slider/Slider.tsx
@@ -0,0 +1,184 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import styles from "./slider.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
+import SwiperCore, {
+  Autoplay,
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+} from "swiper";
+export interface imgList {
+  img: String;
+}
+// SwiperCore.use([Navigation, Autoplay, Pagination]);
+export default function Slider() {
+  const list = [
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+    {
+      img: "https://www.w3schools.com/w3css/img_lights.jpg",
+    },
+  ];
+  const listTitle = [
+    {
+      img: "HUMANS BEHIND THE GAME",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+    {
+      img: "Join the game",
+    },
+  ];
+  const contents = [
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+    {
+      name: " Andrei Streche is a game producer at Gameloft Bucharest on one of our ambitious, unannounced, cross-platform games.",
+    },
+  ];
+  const [currentEvent, setCurrentEvent] = useState(0);
+  return (
+    <>
+    <div className={styles['title-header']}>Our Latest News</div>
+    <div className={styles['content-header']}>Check out what’s new at Gameloft! Deep dive into the latest news on your <br></br>favorite games, as well as stories from Gamelofters all over the world.</div>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        spaceBetween={30}
+        slidesPerView={4}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: true,
+        }}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
+        className={styles.swiper}
+        onInit={(swiper) => {
+          setCurrentEvent(swiper.activeIndex);
+          console.log("first");
+        }}
+        // onSlideChange={(swiper) => {
+        //   setCurrentEvent(swiper.activeIndex);
+        // }}
+        onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
+      >
+         
+        {list.map((item, i) => {
+          return (
+            <>
+           
+            <SwiperSlide key={i} className={styles["wiper-slide"]}>
+              
+              <div className={styles["wiper-slide_wrapper"]}>
+                <div className={styles["wiper-slide_wrapper-header"]}>
+                  {listTitle[i].img}
+                  <div>-----------------</div>
+                  <div>{contents[i].name}</div>
+                </div>
+
+                <div>
+                  <img className={styles["img"]} src={item.img}></img>
+                </div>
+                <div className={styles["title"]}></div>
+                <div className={styles["wiper-slide_wrapper__title"]}>
+                  {listTitle[i].img}
+                </div>
+                <div className={styles["wiper-slide_wrapper-footer"]}>
+                  <button className={styles["wiper-slide_wrapper-footer__btn"]}>
+                    Read More
+                  </button>
+                </div>
+              </div>
+            </SwiperSlide>
+            </>
+          );
+        })}
+      </Swiper>
+    </>
+  );
+}
