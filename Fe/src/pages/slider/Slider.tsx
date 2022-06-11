16e30069daa18f372467d4e9ff956c0b9bd38767
damanh1905 <18130005@st.hcmuaf.edu.vn>
2022-06-11 11:20:19 +0700

    -updateNewFilterSearch

--- Fe/src/pages/slider/Slider.tsx
+++ Fe/src/pages/slider/Slider.tsx
@@ -2,7 +2,12 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import styles from "./slider.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import {
+  faFilter,
+  faAngleDown,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -121,64 +126,103 @@ export default function Slider() {
   const [currentEvent, setCurrentEvent] = useState(0);
   return (
     <>
-    <div className={styles['title-header']}>Our Latest News</div>
-    <div className={styles['content-header']}>Check out what’s new at Gameloft! Deep dive into the latest news on your <br></br>favorite games, as well as stories from Gamelofters all over the world.</div>
-      <Swiper
-        cssMode={true}
-        navigation={true}
-        spaceBetween={30}
-        slidesPerView={4}
-        pagination={true}
-        mousewheel={true}
-        keyboard={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: true,
-        }}
-        modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
-        className={styles.swiper}
-        onInit={(swiper) => {
-          setCurrentEvent(swiper.activeIndex);
-          console.log("first");
-        }}
-        // onSlideChange={(swiper) => {
-        //   setCurrentEvent(swiper.activeIndex);
-        // }}
-        onSlideChange={() => console.log("slide change")}
-        // onSwiper={(swiper) => console.log(swiper)}
-      >
-         
-        {list.map((item, i) => {
-          return (
-            <>
-           
-            <SwiperSlide key={i} className={styles["wiper-slide"]}>
-              
-              <div className={styles["wiper-slide_wrapper"]}>
-                <div className={styles["wiper-slide_wrapper-header"]}>
-                  {listTitle[i].img}
-                  <div>-----------------</div>
-                  <div>{contents[i].name}</div>
-                </div>
+      <div className={styles["container"]}>
+        {" "}
+        <div className={styles["wrapper"]}>
+          <div className={styles["wrapper-left"]}>
+            {" "}
+            <div className={styles["title-header"]}>Our Latest News</div>
+            <div className={styles["content-header"]}>
+              Check out what’s new at Gameloft! Deep dive into the latest news
+              on your <br></br>favorite games, as well as stories from
+              Gamelofters all over the world.
+            </div>
+          </div>
 
+          <div className={styles["wrapper-right"]}>
+            <div className={styles["wrapper-right__filter"]}>
+              <button className={styles["wrapper-right__filter-btn"]}>
                 <div>
-                  <img className={styles["img"]} src={item.img}></img>
+                  <FontAwesomeIcon
+                    className={styles["wrapper-right__filter-btn__icon"]}
+                    icon={faFilter}
+                  ></FontAwesomeIcon>{" "}
+                  Filter
                 </div>
-                <div className={styles["title"]}></div>
-                <div className={styles["wiper-slide_wrapper__title"]}>
-                  {listTitle[i].img}
-                </div>
-                <div className={styles["wiper-slide_wrapper-footer"]}>
-                  <button className={styles["wiper-slide_wrapper-footer__btn"]}>
-                    Read More
-                  </button>
-                </div>
-              </div>
-            </SwiperSlide>
-            </>
-          );
-        })}
-      </Swiper>
+                <span>
+                  <FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon>
+                </span>
+              </button>
+            </div>
+            <div className={styles["wrapper-right__search"]}>
+              <input className={styles["wrapper-right__search__input"]} />
+              <FontAwesomeIcon
+                className={styles["wrapper-right__search__input-icon"]}
+                icon={faMagnifyingGlass}
+              />
+            </div>
+          </div>
+        </div>
+        <Swiper
+          cssMode={true}
+          navigation={true}
+          spaceBetween={30}
+          slidesPerView={4}
+          pagination={true}
+          mousewheel={true}
+          keyboard={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: true,
+          }}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
+          className={styles.swiper}
+          onInit={(swiper) => {
+            setCurrentEvent(swiper.activeIndex);
+            console.log("first");
+          }}
+          // onSlideChange={(swiper) => {
+          //   setCurrentEvent(swiper.activeIndex);
+          // }}
+          onSlideChange={() => console.log("slide change")}
+
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          {list.map((item, i) => {
+            return (
+              <>
+                <SwiperSlide key={i} className={styles["wiper-slide"]}>
+                  <div className={styles["wiper-slide_wrapper"]}>
+                    <div className={styles["wiper-slide_wrapper-header"]}>
+                      {listTitle[i].img}
+                      <div>-----------------</div>
+                      <div>{contents[i].name}</div>
+                    </div>
+
+                    <div>
+                      <img
+                        className={styles["wiper-slide_wrapper__img"]}
+                        src={item.img}
+                      ></img>
+                    </div>
+                    <div className={styles["wiper-slide_wrapper--pink"]}></div>
+                    <div className={styles["wiper-slide_wrapper__title"]}>
+                      {listTitle[i].img}
+                    </div>
+                    <div className={styles["wiper-slide_wrapper-footer"]}>
+                      <button
+                        className={styles["wiper-slide_wrapper-footer__btn"]}
+                      >
+                        Read More
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </>
+            );
+          })}
+        </Swiper>
+      </div>
     </>
   );
 }
