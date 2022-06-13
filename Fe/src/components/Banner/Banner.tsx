f83cdc917624ff22c21b85be51ebfe1bdbe91679
damanh1905 <18130005@st.hcmuaf.edu.vn>
2022-06-13 15:29:16 +0700

    -update fix name banner

--- Fe/src/components/Banner/Banner.tsx
+++ Fe/src/components/Banner/Banner.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import styles from './banner.module.scss';
+import React from "react";
+import styles from "./Banner.module.scss";
 export interface BannerProps {}
 
 export interface DataType {}
 
-const Banner:React.FC = () => {
-  return(
+const Banner: React.FC = () => {
+  return (
     <div className={styles["banner"]}>
-              <button className={styles["button-banner"]}>Watch Trailer</button>
+      <button className={styles["button-banner"]}>Watch Trailer</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default Banner;
