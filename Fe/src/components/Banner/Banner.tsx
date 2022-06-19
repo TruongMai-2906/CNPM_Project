a3fb3dfec88df347e57f8ea0dc2474470512d62f
Hiep299 <85495849+Hiep299@users.noreply.github.com>
2022-06-19 18:38:59 +0700

    upload

--- Fe/src/components/Banner/Banner.tsx
+++ Fe/src/components/Banner/Banner.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import styles from "./Banner.module.scss";
+import  "./banner.scss";
+import ModalVideo from "react-modal-video";
+import Imager from "../../assets/images/home/banner1.jpg";
+import Logo from "../../assets/images/home/logobanner.png";
+
+import {useState} from 'react'
 export interface BannerProps {}
 
-export interface DataType {}
+export interface DataType {
+  background?:{
+    url?:string
+  }
+  backgroundMobile?:{
+    url?:string
+  }
+  logo?:{
+    url?:string
+  }
+  description?:string
+  buttonText?:string
+  link?:string
+}
 
-const Banner: React.FC = () => {
+const Banner: React.FC<DataType & BannerProps> = (props) => {
+  const [isOpen, setOpen] = useState(false)
+  const data:DataType={
+      background:{url:Imager},
+      backgroundMobile:{url:Imager},
+      logo:{url:Logo},
+      description:"March of Empires",
+      buttonText:'Watch Trailer',
+      link:'linkkk'
+  }
   return (
-    <div className={styles["banner"]}>
-      <button className={styles["button-banner"]}>Watch Trailer</button>
+    <div className="banner">  
+      <img src={data?.logo?.url} className="banner_logo" alt="" />
+      <h2 className="banner_description">{data?.description||props.description||""}</h2>
+      <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="J8W4Lm1nTWk" onClose={() => setOpen(false)} />
+			<button className="banner_button" onClick={()=> setOpen(true)}>{props?.buttonText||data?.buttonText||""}</button>
     </div>
   );
 };
--- Fe/src/pages/Register/Register.tsx
+++ Fe/src/pages/Register/Register.tsx
@@ -1,33 +1,32 @@
-import styles from "./register.module.scss";
+import   "./register.scss";
 const Register: React.FC = () => {
   return (
-    <div className={styles["register"]}>
-      <div className={styles["left_register"]}></div>
-      <div className={styles["right_register"]}>
-        <div className={styles["form_register"]}>
-          <div className={styles["form_title"]}>
+    <div className="register">
+      <div className="left_register"></div>
+      <div className="right_register">
+        <div className="form_register">
+          <div className="form_title">
             Keep up with the Gameloft realm!
-            <div className={styles["form_description"]}>
-              Sign up to our newstetter to receive the latest new ,special event
-              and other exciting with me
+            <div className="form_description">
+              Sign up to our newstetter to receive the latest new, special event
+              and other exciting with me.
             </div>
             <input type="text" placeholder="Username" />
             <input type="text" placeholder="Password" />
             <input type="text" placeholder="Repeat Password" />
-            <div className={styles["container_national-date"]}>
-              <input type="date" className={styles["input-date"]} />
-              <select name="VietNam" id="" className={styles["national"]}>
+            <div className="container_national-date">
+              <input type="date" className="input-date" />
+              <select name="VietNam" id="" className="national">
                 VietNam
                 <option value="VietNam">VietNam</option>
               </select>
             </div>
-            <div className={styles["accept_me"]}>
+            <div className="accept_me">
               <input type="checkbox" />
-              <p>
-                {" "}
-                Sign up to our newstetter to receive the latest new ,special
-                event and other exciting with me
-              </p>
+                <p className="register_description">
+                  Sign up to our newstetter to receive the latest new special
+                  event and other exciting with me
+                </p>
             </div>
           </div>
         </div>
