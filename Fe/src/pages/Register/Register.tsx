46e66042f121ef63540ef8fbce326a2e5ce7da8c
Hiep299 <85495849+Hiep299@users.noreply.github.com>
2022-06-20 14:13:27 +0700

    add type regisgter

--- Fe/src/pages/Register/Register.tsx
+++ Fe/src/pages/Register/Register.tsx
@@ -1,15 +1,32 @@
 import   "./register.scss";
-const Register: React.FC = () => {
+export interface RegisterProps {}
+
+export interface DataType {
+    title?:string,
+    newsletter?:string,
+    selectText?:string,
+    optionText?:string,
+    description?:string,
+    buttonText?:string
+}
+const Register: React.FC<DataType & RegisterProps> = (props) => {
+  const data:DataType={
+    title:"Keep up with the Gameloft realm!",
+    newsletter:"Sign up to our newstetter to receive the latest new, special event and other exciting with me.",
+    selectText:"VietNam",
+    optionText:"VietNam",
+    description:"Sign up to our newstetter to receive the latest new special event and other exciting with me",
+    buttonText:"Register"
+}
   return (
     <div className="register">
       <div className="left_register"></div>
       <div className="right_register">
         <div className="form_register">
           <div className="form_title">
-            Keep up with the Gameloft realm!
+          {props?.title||data?.title||""}
             <div className="form_description">
-              Sign up to our newstetter to receive the latest new, special event
-              and other exciting with me.
+            {props?.newsletter||data?.newsletter||""}
             </div>
             <input type="text" placeholder="Username" />
             <input type="text" placeholder="Password" />
@@ -17,20 +34,19 @@ const Register: React.FC = () => {
             <div className="container_national-date">
               <input type="date" className="input-date" />
               <select name="VietNam" id="" className="national">
-                VietNam
-                <option value="VietNam">VietNam</option>
+                  {props?.selectText||data?.selectText||""}
+                <option value="VietNam"> {props?.optionText||data?.optionText||""}</option>
               </select>
             </div>
             <div className="accept_me">
               <input type="checkbox" />
                 <p className="register_description">
-                  Sign up to our newstetter to receive the latest new special
-                  event and other exciting with me
+                {props?.description||data?.description||""}
                 </p>
             </div>
           </div>
         </div>
-        <button>Register</button>
+        <button>  {props?.buttonText||data?.buttonText||""}</button>
       </div>
     </div>
   );
